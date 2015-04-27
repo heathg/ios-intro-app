14f9e3e16806cef23bf1f3b894621731e204d396
heathg <Heathglover@Heaths-MacBook-Pro.local>
2015-04-27 13:03:07 +1000

    Revert 1d05168..b1d7780
    This rolls back to commit 1d051682721a7cc589d1e22c6a1ca79832e8c514.

--- ToDoList/ToDoList/AppDelegate.h
+++ ToDoList/ToDoList/AppDelegate.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  AppDelegate.h
 //  ToDoList
 //
 //  Created by Heath Glover on 26/04/2015.
@@ -8,7 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
 
 
 @end
---
+++ ToDoList/ToDoList/ToDoListTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  ToDoListTableViewController.h
+//  ToDoList
+//
+//  Created by Heath Glover on 26/04/2015.
+//  Copyright (c) 2015 HeathG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToDoListTableViewController : UITableViewController
+- (IBAction)unwindToList:(UIStoryboardSegue *)segue;
+
+@end
